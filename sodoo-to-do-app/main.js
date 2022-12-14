61811d29c4070f7be610a7ad5acf6ea8d04899a7
Morning <morning@admins-iMac-3.local>
2022-12-14 12:41:40 +0800

    up

--- sodoo-to-do-app/main.js
+++ sodoo-to-do-app/main.js
@@ -1,44 +1,57 @@
-// add function
+let now = new Date;
+document.getElementById(myTimer.innerHTML = 'Он сар өдөр' + ' ' + now.getFullYear() + '-' + now.getMonth() + '-' + now.getDay())
 
-function add(x, y) {
-    return x + y;
+
+let task = document.getElementById('list');
+console.log(task.value)
+
+
+
+
+
+let toDoArr = [];
+
+
+function addTask() {
+    // document.getElementById('taskList').innerHTML = task.value + 'delete';
+    let p = document.createElement('p')
+    p.id = toDoArr.length + 1
+
+    // p.innerHTML = task.value + ' <button>edit</button>' + ' <button>done</button>' + ' <button id="del-btn">del</button>';
+    p.innerHTML = `<input value=${task.value} disabled> <button>edit</button> <button>done</button>  <button id="del-btn">del</button>`;
+    toDoArr.push(task.value)
+    console.log(toDoArr);
+
+    document.getElementById('container').appendChild(p);
+
+
+    document.getElementById('taskNum').innerHTML = `${toDoArr.length} task uldlee`
+
+    // const element = document.getElementById("container");
+    // element.appendChild(listAdded);
 }
 
-// print function
+let button = document.getElementById('add');
 
-function print(f) {
-    let x = 2, y = 3;
-    console.log(f(x, y));
+button.addEventListener('click', addTask);
+
+
+
+
+
+
+
+
+
+// delete function
+
+let delButton = document.getElementById('1');
+
+delButton.addEventListener('click', delP);
+
+
+function delP() {
+    let text = p.innerHTML;
+    text.remove();
 }
 
-// callback function
-
-print(add);
-
-
-
-// setTimeout
-
-let x = () => {
-    console.log('arrow function');
-}
-
-
-
-setTimeout(x, 2000); // zaasan hugatsaanii daraa function ajillana
-
-setTimeout(() => { console.log('arrow function'); }, 1000)
-
-
-
-
-function startInterval() {
-    let now = new Date;
-    let myTimer = document.getElementById('myTimer')
-    myTimer.innerHTML = 'Он сар өдөр' + ' ' + now.getFullYear() + '-' + now.getMonth() + '-' + now.getDay();
-}
-
-setInterval(startInterval, 10);
-
-
-document.getElementById('myTimer')
