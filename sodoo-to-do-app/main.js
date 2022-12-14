e035f0de59279ed83d8ab5e96f03eadb2fa3ba0e
Morning <morning@admins-iMac-3.local>
2022-12-14 13:47:58 +0800

    up

--- sodoo-to-do-app/main.js
+++ sodoo-to-do-app/main.js
@@ -18,7 +18,7 @@ function addTask() {
     p.id = toDoArr.length + 1
 
     // p.innerHTML = task.value + ' <button>edit</button>' + ' <button>done</button>' + ' <button id="del-btn">del</button>';
-    p.innerHTML = `<input value=${task.value} disabled> <button>edit</button> <button>done</button>  <button id="del-btn">del</button>`;
+    p.innerHTML = `<input value=${task.value} disabled> <button>edit</button> <button>done</button>  <button id="del-btn-${toDoArr.length + 1} " onclick="delP()">del</button>`;
     toDoArr.push(task.value)
     console.log(toDoArr);
 
@@ -29,6 +29,26 @@ function addTask() {
 
     // const element = document.getElementById("container");
     // element.appendChild(listAdded);
+
+
+
+
+
+
+    // let delButton = document.getElementById('del-btn-1');
+
+    // delButton.addEventListener('click', delP);
+
+
+    // function delP() {
+    //     const element = document.getElementById('1');
+    //     // document.getElementById('1').remove();
+    //     element.remove();
+    //     console.log('clicked');
+    //     document.getElementById('1').remove;
+    // }
+
+
 }
 
 let button = document.getElementById('add');
@@ -45,13 +65,15 @@ button.addEventListener('click', addTask);
 
 // delete function
 
-let delButton = document.getElementById('1');
+let delButton = document.getElementById('del-btn-1');
 
 delButton.addEventListener('click', delP);
 
 
 function delP() {
-    let text = p.innerHTML;
-    text.remove();
+    console.log('deleted');
+    // const element = document.getElementById('1');
+    document.getElementById('1').remove();
+    // element.removeChild(list.firstElementChild);
 }
 
