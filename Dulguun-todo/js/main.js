c79bdee2f5b4790807361b4e900d8da5dd1f3ab4
Joyride77 <nfive8@gmail.com>
2022-12-15 07:54:18 +0800

    add

--- Dulguun-todo/js/main.js
+++ Dulguun-todo/js/main.js
@@ -16,6 +16,8 @@ let todoCreate = document.getElementById('todo-create');
 let todoList = document.getElementById('todo-list');
 let button = document.getElementById('add');
 
+let toDoList = [];
+
 
 button.addEventListener('click', () => {
 
@@ -27,3 +29,14 @@ button.addEventListener('click', () => {
 
 })
 
+
+function passValue (value, count) {
+
+    let n = `<div class="input-group mb-3">
+            <input type="text" class="form-control" name="task" value=${value} id="input" disabled>
+            <button class="btn btn-warning btn-outline-secondary" type="button" id="editBtn">+ Edit</button>
+            <button class="btn btn-warning btn-outline-secondary" type="button" id="saveBtn" disabled> Confirm</button>
+            <button class="btn btn-warning btn-outline-secondary" type="button" id="removeBtn" onclick="deleteTask()"> Delete</button>
+            </div>`
+    return n;
+}
