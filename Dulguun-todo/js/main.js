f3c23108d73f69dfb503de000db451d0698cbe26
Morning <morning@mstars-lab2-08s-iMac.local>
2022-12-15 12:17:45 +0800

    add

--- Dulguun-todo/js/main.js
+++ Dulguun-todo/js/main.js
@@ -1,5 +1,3 @@
-console.log("Todo list project");
-console.log("Todo list project #2");
 // Date
 
 let now = new Date();
@@ -18,31 +16,36 @@ let todoCreate = document.getElementById('todo-create');
 let todoList = document.getElementById('todo-list');
 let button = document.getElementById('add');
 
-let list = [];
+function editFunc(){
+    console.log('edit button clicked');
+}
+
+function saveFunc(){
+    console.log('save button clicked');
+}
+function deleteFunc(event){
+    console.log(event);
+    console.log('delete button clicked');
+}
 
 
 button.addEventListener('click', () => {
-
-    let input = document.createElement('p');
+    let randomIndex = Math.floor(Math.random()*100);
+    let input = document.createElement('div');
     input.innerHTML = myValue(todoCreate.value);;
 
     todoList.appendChild(input);
 })
 
-function myValue (arr) {
+function myValue (value) {
     let i = `<div class="input-group mb-3">
-            <input type="text" class="form-control" name="task" value=${arr} id="input" disabled>
+            <input type="text" class="form-control" name="task" value=${value} id="input" disabled>
 
-            <button class="btn btn-warning" type="button" id="editBtn">+ Edit</button>
+            <button class="btn btn-warning" type="button" id="editBtn" onclick="editFunc(this)">+ Edit</button>
 
-            <button class="btn btn-warning" type="button" id="saveBtn" disabled>Confirm</button>
+            <button class="btn btn-warning" type="button" id="saveBtn" onclick="saveFunc(this)" disabled>Confirm</button>
 
-            <button class="btn btn-warning" type="button" id="removeBtn">Delete</button>
+            <button class="btn btn-warning" type="button" id="deleteBtn" onclick="deleteFunc(this)">Delete</button>
             </div>`
     return i;
 }
-
-let myRemove = document.getElementById('removeBtn');
-myRemove.addEventListener('click', () => {
-    input.remove();
-})
