bde0d3c07a70c746f62e6faa41def8cfd0cb7e2d
Morning <morning@mtars-lab2-17s-iMac.local>
2022-12-15 12:38:18 +0800

    project

--- otgonmunkhTodoapp/js/main.js
+++ otgonmunkhTodoapp/js/main.js
@@ -15,21 +15,39 @@ mytimer.innerHTML = `Он сар өдөр: ${fullYear}-${month}-${day}`
 
 let found = document.getElementById("myInput");
 console.log(found);
-let button  = document.getElementById("control");
-console.log(button );
+let button = document.getElementById("control");
+console.log(button);
 let todolist = document.getElementById("dish");
 console.log(todolist);
 
+function editFunc() {
+    console.log("edit button clicked");
+}
+function saveFunc() {
+    console.log("save button clicked");
+}
+function deleteFunc(event) {
+    console.log(event);
+    console.log(event.parentNode);
+    event.parentNode.remove(event);
+    console.log("delete button clicked");
+
+}
 button.addEventListener('click', () => {
     console.log("control clicked");
     console.log(found.value);
     let input = document.createElement("div");
-    input.innerHTML = `<input value=${found.value}  disabled  
-    <button class="btn btn-border-radius-none"><button/>
-    <button class="btn btn-border-radius-none"></button>`
+    let randomIndex = Math.floor(Math.random() * 100);
+    input.id="con_${randomIndex}"
+    document.getElementById("con_${randomIndex}")="background-color: grey"
+    input.innerHTML = `<input value=${found.value}  disabled >
+    
+    <button onclick="editFunc()" id="edit_${randomIndex}" class="btn btn-border-none bi bi-pencil-fill"></button>
+    <button onclick="saveFunc()" class="btn btn-border-none bi bi-check-lg"></button>
+    <button onclick="deleteFunc(this)" id="edit_${randomIndex}" class="btn btn-border-none bi bi-trash"></button>`
 
     todolist.appendChild(input);
-   
+
 })
 
 
