dc6bb5bc6390f480e7d12b0cf36e8f75a1d85391
Morning <morning@admins-iMac-3.local>
2022-12-15 12:37:54 +0800

    12/15/2022 overhaul

--- bilguun.e-to-do-app/project/js/toDo.js
+++ bilguun.e-to-do-app/project/js/toDo.js
@@ -2,32 +2,21 @@
 function startInterval() {
     let now = new Date()
     let myTimer = document.getElementById("myTimer")
-    setInterval(startInterval, 1000)
-    myTimer.innerHTML = `Today's date: ${now.getFullYear()}/${now.getDate()}/${now.getDay()}`
+    // setInterval(startInterval, 1000)
+    myTimer.innerHTML = `Today's date: ${now.getDate()}/${now.getMonth()+1}/${now.getFullYear()}`
 }
+
+startInterval()
 let inputText = document.getElementById("inputText")
 
 let submitBtn = document.getElementById("submitBtn")
 let toDoInput = document.getElementById("toDoInput");
 let taskL = document.getElementById('taskList');
-let value;
-submitBtn.addEventListener("click", () => {
-    let value = toDoInput.value
-    console.log(value)
-    let p = document.createElement("p")
-    let div = document.createElement("div")
-    p.textContent = value
-    inputText.appendChild(p);
-    inputText.appendChild(div);
-    p.innerHTML = `${value}`
-    div.innerHTML = `<button class="btn btn-warning">Edit </button> <button class="btn btn-success">Check</button> <button class="btn btn-danger">Delete</button>`
-
-    // task container
-    // let taskContainer = document.createElement('div');
-    // taskContainer.classList.add('task');
-
-    // taskL.appendChild(taskContainer);
-
-})
+function addFunc() {
+    let todoElement = document.createElement("div")
+    todoElement.innerHTML = `TODO <input value="${toDoInput.value}" class=" rounded w-25" > <button onclick="editFunc()" class="btn btn-warning">EDIT</button> <button onclick="saveFunc()" class="btn btn-success">CHECK</button> <button onclick="deleteFunc(this)" class="btn btn-danger">DELETE</button>`
+    inputText.appendChild(todoElement)
+}
 
 
+submitBtn.addEventListener("click", addFunc)
