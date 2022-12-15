00d9bd3eeca85194ea6b54368bd988d81cd38100
Morning <morning@mstars-lab2-02s-iMac.local>
2022-12-15 09:36:05 +0800

    /\/\/\/\/\/\/\/\/\/\/

--- Tserenpuntsag/js/main.js
+++ Tserenpuntsag/js/main.js
@@ -6,14 +6,11 @@ let todoCreate = document.getElementById("todoCreate")
 let button = document.getElementById("button");
 
 button.addEventListener("click", () => {
-    console.log("add clicked")
     console.log(todoCreate.value)
 
     let input = document.createElement("p");
 
     let edit = document.createElement("img")
-
-    
 
     input.innerHTML = `<input value=${todoCreate.value} disabled> <button> <img src="./img/edit.svg" alt="edit" style="width: 10px; heigth: 10px"></button> <button> <img src="./img/correct.svg" alt="correct" style="width: 10px; heigth: 10px"> </button> <button> <img src="./img/delete.svg" alt="delete" style="width: 10px; heigth: 10px"> </button>`;
 
