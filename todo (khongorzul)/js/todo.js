b27513342c6b79057d6d5683c54f78c6bad6548b
WkhongorzulW <bkhongorzul064@gmail.com>
2022-12-15 12:39:46 +0800

    todolist day-2

--- todo (khongorzul)/js/todo.js
+++ todo (khongorzul)/js/todo.js
@@ -7,26 +7,26 @@ let addInput = document.getElementById("addInput");
 let toDo = document.getElementById("toDo");
 
 
+
+
+function deleteFunc(event){
+    event.parentNode.remove(event);
+};
+
 addButton.addEventListener("click", () => {
     let input = document.createElement("p");
-    console.log(addInput.value)
-    input.innerHTML = `<input id="listInput" style="border: none; background-color: white" value="${addInput.value}" disabled>
+    console.log(addInput.value);
+    let randomIndex = Math.floor(Math.random()*100);
+    input.innerHTML = `<input id="listInput_${randomIndex}" style="border: none; background-color: white" value="${addInput.value}" disabled>
      <button id="editButton" style="border-radius: 10px; border-color: yellow; background-color: white"><i class="fa-solid fa-pen-to-square"></i></button>
-     <button id="checkButton" style="border-radius: 10px; border-color: yellow;  background-color: white" onclick="checkFunc(this)"><i style="color: green" class="fa-solid fa-check"></i></button> 
-     <button id="trashButton" style="border-radius: 10px; border-color: yellow;  background-color: white" onclick="deleteFunc()"><i style="color: red" class="fa-regular fa-trash-can"></i></button>`;
+     <button id="checkButton_${randomIndex}" style="border-radius: 10px; border-color: yellow;  background-color: white" onclick="checkFunc()"><i style="color: green" class="fa-solid fa-check"></i></button> 
+     <button id="${randomIndex}" style="border-radius: 10px; border-color: yellow;  background-color: white" onclick="deleteFunc(this)"><i style="color: red" class="fa-regular fa-trash-can"></i></button>`;
     toDo.appendChild(input);
     input.style = "border: 1px solid orange; border-radius: 20px; padding: 15px";
 
-    let editButton = document.getElementById("editButton");
     let listInput = document.getElementById("listInput");
-
+    let editButton = document.getElementById("editButton");
     editButton.addEventListener("click", () => {
         listInput.disabled = false;
-        editButton.innerHTML = `<i class="fa-solid fa-file-contract"></i>`
-    });
-
-    let checkButton = document.getElementById("checkButton");
-    checkButton.addEventListener("click", () => {
-        document.removeChild(listInput);
-    });
+    })
 });
