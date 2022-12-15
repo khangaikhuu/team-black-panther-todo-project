d283353c25498561764d1d79fc15e93a782de259
Morning <morning@mstars-lab2-08s-iMac.local>
2022-12-15 09:03:04 +0800

    add

--- Dulguun-todo/js/main.js
+++ Dulguun-todo/js/main.js
@@ -6,7 +6,7 @@ let months = now.getMonth()+1;
 let days = now.getDate();
 
 let myTimer = document.getElementById('myTimer');
-myTimer.innerHTML = `Он сар өдөр: ${years}-${months}-${days}`;
+myTimer.innerHTML = `Year Month Days: ${years}-${months}-${days}`;
 
 
 
@@ -16,27 +16,34 @@ let todoCreate = document.getElementById('todo-create');
 let todoList = document.getElementById('todo-list');
 let button = document.getElementById('add');
 
-let toDoList = [];
+let list = [];
 
 
 button.addEventListener('click', () => {
 
     let input = document.createElement('p');
-    input.innerHTML = `<input value=${todoCreate.value}> <button>Edit</button> <button>Del</button>`;
+    input.innerHTML = myValue(todoCreate.value);;
 
     todoList.appendChild(input);
-    
-
 })
 
-
-function passValue (value, count) {
-
+function myValue (arr) {
     let n = `<div class="input-group mb-3">
-            <input type="text" class="form-control" name="task" value=${value} id="input" disabled>
+            <input type="text" class="form-control" name="task" value=${arr} id="input" disabled>
             <button class="btn btn-warning btn-outline-secondary" type="button" id="editBtn">+ Edit</button>
             <button class="btn btn-warning btn-outline-secondary" type="button" id="saveBtn" disabled> Confirm</button>
             <button class="btn btn-warning btn-outline-secondary" type="button" id="removeBtn" onclick="deleteTask()"> Delete</button>
             </div>`
     return n;
 }
+
+const myRemove = document.getElementById('removeBtn');
+myRemove.addEventListener('click', () => {
+    myRemove.remove();
+})
+
+// for (const myRemove of myRemoves) {
+//     myRemove.addEventListener('click', (event) => {
+//       event.target.remove();
+//     });
+//   }
