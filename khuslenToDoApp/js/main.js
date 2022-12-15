4e0554def799d0969cea832fa9e5c4275453f63f
Morning <morning@Lab-02-01s-iMac.local>
2022-12-15 09:24:25 +0800

    hello world

--- khuslenToDoApp/js/main.js
+++ khuslenToDoApp/js/main.js
@@ -11,31 +11,34 @@ let p = document.createElement("p");
 console.log(p)
 let todoCreate = document.getElementById("todo-create");
 
-
+ 
 // style нэмж оруулав
 
 button.style = "padding: 10px 20px; border: 2px solid silver; border-radius: 5px; width: 100px"
 button.style.backgroundColor = "gold"
 todoCreate.style = "padding: 10px 20px; border: 2px solid silver; border-radius: 5px; width: 200px"
 
-let too = 0;
+// let too = 0;
 // let edit1 = document.getElementById("edit")
 
 // let input = document.createElement('p');
 
 button.addEventListener("click", () => {
-    too = too + 1;
     let input = document.createElement('p');
     input.innerHTML = `<input class = "py-2 px-4 rounded" value="${todoCreate.value}" disabled><button id="edit" class=" py-2 px-4 rounded bg-primary"><i class='fas fa-pencil bg-primary'></i></button><button class=" py-2 px-4 rounded bg-info"><i class='fas fa-check bg-info'></i></button><button class=" py-2 px-4 rounded bg-danger"><i class='fas fa-trash bg-danger'></i></button>`;
-    // console.log(edit);
-    // console.log(too)
+
+    // let btn1 = document.createElement("button")
+
+    // btn1.innerHTML = 
 
     todo.appendChild(input);
 })
 
-// let edit = document.getElementById(edit);
+// let edit = document.getElementById("edit");
+
 
 // edit.addEventListener("click", () => {
+    
 //     document.querySelector('#button').disabled = false;
 // })
 
