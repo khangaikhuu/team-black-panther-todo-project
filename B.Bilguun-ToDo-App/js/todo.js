3ac1d92cdaf9dba80182ed7fc63a7f23a6bdbdd2
ruzishdee <bilguunbayanmunkh10@gmail.com>
2022-12-18 15:37:28 +0800

    del button edited

--- B.Bilguun-ToDo-App/js/todo.js
+++ B.Bilguun-ToDo-App/js/todo.js
@@ -22,10 +22,9 @@ button.addEventListener("click", () => {
 
     let list = document.createElement('div');
 
-
     list.innerHTML = `<input id="secInput" value="${todoCreate.value}" disabled> 
-    <button onclick="editClicked(this)">edit</button>
-     <button onclick="doneClicked(this)">done</button> 
+    <button onclick="editFunc(this)">edit</button>
+     <button >done</button> 
      <button onclick="delClicked(this)">del</button>`;
     // edit.push(todoList.value);
     todoList.appendChild(list);
@@ -40,17 +39,28 @@ let editbutton = document.getElementById('edButton');
 //     console.log("clicked");
 // });
 // let secInput = [];
-function editClicked(event) {
-    console.log('button clicked')
-    console.log(event.parentNode);
-    event.parentNode.childNodes[0].disabled = false;
-    // document.getElementById("secInput").disabled = false;
-}
 
-function doneClicked(eve) {
-    console.log('button done clicked')
-    console.log(eve.parentNode);
-    eve.parentNode.childNodes[0].disabled = true;
+
+// function editClicked(event) {
+//     console.log('button clicked')
+//     console.log(event.parentNode);
+//     event.parentNode.childNodes[0].disabled = false;
+//     // document.getElementById("secInput").disabled = false;
+// }
+
+// function doneClicked(eve) {
+//     console.log('button done clicked')
+//     console.log(eve.parentNode);
+//     eve.parentNode.childNodes[0].disabled = true;
+// }
+
+function editFunc(event) {
+    console.log('button clicked');
+    if (event.parentNode.childNodes[0].disabled === true) {
+        event.parentNode.childNodes[0].disabled = false;
+    } else {
+        event.parentNode.childNodes[0].disabled = true;
+    }
 }
 
 function delClicked(event) {
