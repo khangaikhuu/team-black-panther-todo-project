ee54262cceed9864c4ff4051ea8862edf9523eba
ruzishdee <bilguunbayanmunkh10@gmail.com>
2022-12-18 20:47:32 +0800

    button editing

--- B.Bilguun-ToDo-App/js/todo.js
+++ B.Bilguun-ToDo-App/js/todo.js
@@ -5,6 +5,10 @@ myTimer.innerHTML = `${now.getFullYear()} - ${
   now.getMonth() + 1
 } - ${now.getUTCDate()}`;
 
+
+// Count task
+
+
 // Button to add list
 let todoCreate = document.getElementById("myInput");
 
@@ -15,7 +19,7 @@ let button = document.getElementById("addButton");
 
 
 
-// let edit = [];
+let edit = [];
 
 button.addEventListener("click", () => {
     console.log("clicked");
@@ -23,48 +27,42 @@ button.addEventListener("click", () => {
     let list = document.createElement('div');
 
     list.innerHTML = `<input id="secInput" value="${todoCreate.value}" disabled> 
-    <button onclick="editFunc(this)">edit</button>
-     <button >done</button> 
+    <button onclick="editFunc(this)"><img src="./images/pencil-fill.svg" alt=""></button>
+     <button onclick="doneFunc()">done</button> 
      <button onclick="delClicked(this)">del</button>`;
     // edit.push(todoList.value);
     todoList.appendChild(list);
 
+    edit.push(todoCreate.value);
+    document.getElementById('task').innerHTML = `${edit.length} task үлдлээ !`
 });
-
-// let secInput = document.getElementById("secInput");
-
-let editbutton = document.getElementById('edButton');
-
-// editbutton.addEventListener("click", () => {
-//     console.log("clicked");
-// });
-// let secInput = [];
-
-
-// function editClicked(event) {
-//     console.log('button clicked')
-//     console.log(event.parentNode);
-//     event.parentNode.childNodes[0].disabled = false;
-//     // document.getElementById("secInput").disabled = false;
-// }
-
-// function doneClicked(eve) {
-//     console.log('button done clicked')
-//     console.log(eve.parentNode);
-//     eve.parentNode.childNodes[0].disabled = true;
-// }
 
 function editFunc(event) {
     console.log('button clicked');
     if (event.parentNode.childNodes[0].disabled === true) {
         event.parentNode.childNodes[0].disabled = false;
+        butsaah = event.nextSibling
+        console.log(butsaah);
+        // event.parentNode.childNodes[1].innerHTML = `<button onclick="editFunc(this)"><img src="./images/save2.svg" alt=""></button>`
     } else {
         event.parentNode.childNodes[0].disabled = true;
+        // event.parentNode.childNodes[1].innerHTML = `<button onclick="editFunc(this)"><img src="./images/pencil-fill.svg" alt=""></button>`
     }
+}
+
+function doneFunc() {
+    console.log('Done clicked');
 }
 
 function delClicked(event) {
     console.log('button done clicked')
     console.log(event.parentNode);
     event.parentNode.remove(event);
+    edit.pop();
+    document.getElementById('task').innerHTML = `${edit.length} task үлдлээ !`
+    if (edit.length === 0) {
+        document.getElementById('task').style = 'display: none;'
+    } else {
+        document.getElementById('task').style = 'display: block;'
+    }
 }
