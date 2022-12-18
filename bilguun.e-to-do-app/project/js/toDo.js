303b5e40f18dc9e9e30bf00ef6f2c55621560727
bilguun-enkh <bilguunenkh21@gmail.com>
2022-12-18 14:37:40 +0800

    12/18/2022 delete button

--- bilguun.e-to-do-app/project/js/toDo.js
+++ bilguun.e-to-do-app/project/js/toDo.js
@@ -3,7 +3,7 @@ function startInterval() {
     let now = new Date()
     let myTimer = document.getElementById("myTimer")
     // setInterval(startInterval, 1000)
-    myTimer.innerHTML = `Today's date: ${now.getDate()}/${now.getMonth()+1}/${now.getFullYear()}`
+    myTimer.innerHTML = `Today's date: ${now.getDate()}/${now.getMonth() + 1}/${now.getFullYear()}`
 }
 
 startInterval()
@@ -14,9 +14,28 @@ let toDoInput = document.getElementById("toDoInput");
 let taskL = document.getElementById('taskList');
 function addFunc() {
     let todoElement = document.createElement("div")
-    todoElement.innerHTML = `TODO <input value="${toDoInput.value}" class=" rounded w-25" > <button onclick="editFunc()" class="btn btn-warning">EDIT</button> <button onclick="saveFunc()" class="btn btn-success">CHECK</button> <button onclick="deleteFunc(this)" class="btn btn-danger">DELETE</button>`
+    todoElement.innerHTML = 
+    `<input value="${toDoInput.value}" disabled id="inputID" class="rounded w-50 mx-3">
+    <button onclick="editFunc()" class="btn btn-warning">EDIT</button>
+    <button onclick="checkFunc()" class="p-2 btn btn-success">CHECK</button>
+    <button onclick="deleteFunc(this)" class="btn btn-danger">DELETE</button>`
     inputText.appendChild(todoElement)
 }
-
-
+function editFunc() {
+    if (inputID.disabled) {
+        inputID.disabled = false
+    } else {
+        inputID.disabled = true
+    }
+}
+function checkFunc() {
+    if(inputID.style.backgroundColor != "green"){
+        inputID.style.backgroundColor = "green"
+    } else {
+        inputID.style.backgroundColor = "white"
+    }
+}
+function deleteFunc(element) {
+    element.parentNode.removeChild(element);
+}
 submitBtn.addEventListener("click", addFunc)
