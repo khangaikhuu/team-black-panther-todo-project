37432eda39088968e6f74d0cb72f9b9a645bf49f
kadr56 <sh.munkhbaatar@gmail.com>
2022-12-18 12:47:13 +0800

    to-do-app completed

--- Munkhbaatar-to-do-app/js/todo.js
+++ Munkhbaatar-to-do-app/js/todo.js
@@ -15,56 +15,126 @@ function date() {
 
 
 let addTaskBtn = document.getElementById('addBtn');
-// let editTaskBtn = document.getElementById('editBtn');
-
-let toDoList = [];
+let toDoListArray = [];
 let newTask = document.getElementById('newTask');
 
 addTaskBtn.addEventListener('click', () => {
-    // console.log('new task = ' + newTask.value);
-
     if(newTask.value == "") {
-        alert("value is empty");
+        alert("Task хоосон байна.");
     }
     else {
         let p = document.createElement('p');
-        p.id = "task-" + toDoList.length + 1;
-        p.innerHTML = passValue(newTask.value, toDoList.length + 1);
+        let randomID = Math.floor((Math.random()*1000))
+        p.id = "task-" + randomID;
+        p.innerHTML = passValue(newTask.value, randomID);
         document.getElementById('list').appendChild(p);
-        toDoList.push(newTask.value);
-        document.getElementById('counter').innerHTML = `${toDoList.length} жагсаалт`;
-        console.log(toDoList);
+        toDoListArray.push([newTask.value, "task-"+randomID, false]);  // task + ID + done(true or false)
+        document.getElementById('counter').innerHTML = `Нийт ${toDoListArray.length} task`;
+        countDoneTask();
+        // console.log(toDoListArray);
+        let newTaskInput = document.getElementById('newTask');
+        newTask.value = '';
+        console.log(toDoListArray);
     }
-    
-
 })
 
+
+function countDoneTask() {
+    let counter = 0;
+    for(let i = 0; i < toDoListArray.length; i++) {
+        if(toDoListArray[i][2] == true ) {
+            counter++;
+        }
+    }
+    document.getElementById('doneCounter').innerHTML = `Хийсэн ${counter} task`;
+}
+
+
+function doneFunc(event) {
+    document.getElementById(event.id).innerHTML.style = `<i class="bi bi-save"></i>`
+    if(event.parentNode.childNodes[1].style.textDecoration == 'line-through') {
+        event.parentNode.childNodes[1].style.textDecoration  = `none`;
+        event.parentNode.childNodes[1].style.color  = `black`;
+        arrDoneTask(event);
+    } else {
+        event.parentNode.childNodes[1].style.textDecoration  = `line-through`;
+        event.parentNode.childNodes[1].style.color  = `red`;
+        arrDoneTask(event);
+    }
+    countDoneTask()
+    console.log("After done task. Array list = ");
+    printTaskArrayList();
+}
+
 function editFunc(event) {
-    // event.parentNode.
-    console.log(event.id);
-    // console.log(event.parentNode.editBtn);
-    event.parentNode.childNodes[1].disabled = false;
-    // console.log(event.parentNode.childNodes[1].disabled);
-    // document.querySelector('#event.id').disabled = false;
+    if(event.parentNode.childNodes[1].disabled)  { // check if input field is disabled.  
+        event.parentNode.childNodes[1].disabled = false;
+        document.getElementById(event.id).innerHTML = `<i class="bi bi-inbox-fill"></i>`
+    } else {   // input field is not disabled. 
+        event.parentNode.childNodes[1].disabled = true; 
+        document.getElementById(event.id).innerHTML = `<i class="bi bi-pencil-fill"></i>`
+        arrEditTask(event);
+    }
+    console.log("After editing task. Array list = ");
+    printTaskArrayList();
 }
 
 function deleteFunc(event) {
-    // event.id; 
-    // console.log(event.id);
-    event.parentNode.remove(event);
-    toDoList.splice(0,1);
-    document.getElementById('counter').innerHTML = `${toDoList.length} жагсаалт`;
+    event.parentNode.parentNode.remove(event);
+    arrRemoveTask(event.parentNode.parentNode.id);
+    document.getElementById('counter').innerHTML = `Нийт ${toDoListArray.length} task`;
 }
 
 
+// Once task is removed, remove it from array list.
+function arrRemoveTask(id) {    
+    console.log("removed id = " + id)
+    for(let i = 0; i < toDoListArray.length; i++) {
+        if(id == toDoListArray[i][1] ) {
+            toDoListArray.splice(i, 1)
+        }
+    }
+    console.log("After removing task. Array list = ");
+    printTaskArrayList();
+}
+
+
+// Once input is edited, make change to the Array.
+function arrEditTask(e) {
+    console.log("Edit id = " + e.parentNode.parentNode.id)
+    let id = e.parentNode.parentNode.id;
+    for(let i = 0; i < toDoListArray.length; i++) {
+        if(id == toDoListArray[i][1] ) {
+            toDoListArray[i][0] = e.parentElement.children[0].value;
+        }
+    }
+}
+
+// Once task is done, make change to the Array.
+function arrDoneTask(e) {
+    console.log("Done id = " + e.parentNode.parentNode.id)
+    let id = e.parentNode.parentNode.id;
+    for(let i = 0; i < toDoListArray.length; i++) {
+        if(id == toDoListArray[i][1] ) {
+            if(toDoListArray[i][2] == true) {
+                toDoListArray[i][2] = false;
+            } else {
+                toDoListArray[i][2] = true;
+            }
+        }
+    }
+}
+
+function printTaskArrayList() {
+    console.log(toDoListArray);
+}
 
 function passValue (value, count) {
-
     let n = `<div class="input-group mb-3">
             <input type="text" class="form-control" name="task" value=${value} id="input" disabled>
-            <button class="btn btn-warning btn-outline-secondary" type="button" id="editBtn-${count}" onclick="editFunc(this)"> <i class="bi bi-pencil-fill"></i></button>
-            <button class="btn btn-warning btn-outline-secondary" type="button" id="saveBtn" onclick="saveFunc(this)" disabled> <i class="bi bi-check2"></i> </button>
-            <button class="btn btn-warning btn-outline-secondary" type="button" id="removeBtn" onclick="deleteFunc(this)"> <i class="bi bi-trash3-fill"></i></button>
+            <button class="btn btn-warning" type="button" id="editBtn-${count}" onclick="editFunc(this)"> <i class="bi bi-pencil-fill"></i></button>
+            <button class="btn btn-success" type="button" id="doneBtn-${count}" onclick="doneFunc(this)"> <i class="bi bi-check2"></i> </button>
+            <button class="btn btn-danger" type="button" id="removeBtn-${count}" onclick="deleteFunc(this)"> <i class="bi bi-trash3-fill"></i></button>
             </div>`
     return n;
 }
