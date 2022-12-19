75fc6cd470a878e12c252f2a1984690f13c5941c
Munkhtuvshin Tuvshin <tuvshin@Munkhtuvshins-MacBook-Pro.local>
2022-12-19 21:06:30 +0800

    btn

--- Tuvshin_Todo_App/js/todo.js
+++ Tuvshin_Todo_App/js/todo.js
@@ -9,18 +9,6 @@ let todoCreate = document.getElementById("todo-create");
 let todoList = document.getElementById("todo-list");
 let button = document.getElementById("add");
 
-//FUNCTION BUTTON
-
-function editFunc(){
-    console.log('edit button clicked');
-}
-function correctFunc(){
-    console.log('save button clicked');
-}
-function deleteFunc(){
-    // console.log()
-    console.log('delete button clicked').remove();
-}
 
 
 
@@ -34,12 +22,36 @@ button.addEventListener('click', () => {
 
     let input = document.createElement('p');
     input.innerHTML = `<input value=${todoCreate.value} disabled> 
-            <button  onclick="editFunc()"> Edit </button> 
-            <button onclick="correctFunc()"> SAVE </button> 
-            <button onclick="deleteFunc(this)"> DEL </button>`;
+            <button onclick="edit(this)" style="width: 50px; height: 30px;margin-left:-6px; background-color: #f49d1a; color: blue" > Edit </button> 
+            <button onclick="correct" style="width: 50px; height: 30px;margin-left:-6px; margin-top: -100px; background-color: #f49d1a; color:green"> SAVE </button> 
+            <button onclick="delete" style="width: 50px; height: 30px;margin-left:-6px; background-color: #f49d1a; color:red"> DEL </button>`;
 
     todoList.appendChild(input);
 
-});
+})
+
+//FUNCTION BUTTON
+
+const edit = (e) => {
+    let p = e.parentNode.firstElementChild;
+    child.removeAttribute("disabled")
+    console.log('edit button', child)
+}
+
+
+
+// function editFunc() {
+//     console.log('edit button clicked');
+// }
+    
+
+// function correctFunc() {
+//     console.log('save button clicked');
+// }
+// function deleteFunc() {
+//     // console.log()
+//     console.log('delete button clicked').remove();
+// }
+
 
 
