1c2c3768f4f8eecfe05afbff91cfe5e48db81cfc
Khangaikhuu <khangaikhuu@gmail.com>
2022-12-19 12:46:25 +0800

    :

--- Khangaikhuu/Day-24/js/exercises.js
+++ Khangaikhuu/Day-24/js/exercises.js
@@ -7,12 +7,15 @@ let arrays = [1, 3, 4, 5, 6, 7, 8];
 
 // Өгөгдсөн array-ний бүх элементүүдээр гүйгээд элемент болгон дээр өгөгдсөн функцыг хэрэгжүүлдэг функц бичнэ үү
 function addition (arr, callback){
-    
+    for(let i = 0; i < arr.length; i++){
+        arr[i] = callback(arr[i])
+    }
 }
 
 
 function addTwo (n){
     return n + 2;
 }
-
+console.log(arrays);
 addition(arrays, addTwo); /// [3, 5, 6, 7, 8, 9, 10]
+console.log(arrays);
