09a9e7a0181cfcaecffb92554ec9fb20b3398cfc
Morning <morning@MStarss-iMac.local>
2022-12-19 11:17:31 +0800

    day24 added

--- Tuvshin_Todo_App/js/todo.js
+++ Tuvshin_Todo_App/js/todo.js
@@ -1,43 +1,3 @@
-// // Date part
-// let presentTime = new Date();
-// let shortDate = ' Он cар өдөр  ' + presentTime.getFullYear() + ' - ' + presentTime.getMonth() + ' - ' + presentTime.getDate();
-// document.getElementById("presentTime").innerHTML = shortDate;
-
-// // TODO APP
-// let todoCreate = document.getElementById("todo-create");
-// let todoList = document.getElementById("todo-list");
-// let button = document.getElementById("add");
-
-// // console.log(button);
-
-
-// function editFunc(){
-//     console.log('edit button clicked');
-// }
-
-// function correctFunc(){
-//     console.log('save button clicked');
-// }
-// function deleteFunc(event){
-//     console.log(event)
-//     console.log('delete button clicked');
-
-
-
-// button.addEventListener('click', () => {
-    
-//     console.log('add clicked');
-//     console.log(todoCreate.value);
-
-
-//     let input = document.createElement('p');
-//     input.innerHTML = `<input value=${todoCreate.value} > 
-//                                     <button id="edit_${randomIndex}" onclick="editFunc()"> EDIT </button> 
-//                                     <button onclick="saveFunc()"> SAVE </button> 
-//                                     <button id="${randomIndex}" onclick="deleteFunc(this)"> DELETE </button>`
-
-//     todoList.appendChild(input);
-// });
 
 // Date part
 let presentTime = new Date();
@@ -49,15 +9,37 @@ let todoCreate = document.getElementById("todo-create");
 let todoList = document.getElementById("todo-list");
 let button = document.getElementById("add");
 
-// console.log(button);
+//FUNCTION BUTTON
+
+function editFunc(){
+    console.log('edit button clicked');
+}
+function correctFunc(){
+    console.log('save button clicked');
+}
+function deleteFunc(){
+    // console.log()
+    console.log('delete button clicked').remove();
+}
+
+
+
+
+
+// BUTTON FUNCTION
 
 button.addEventListener('click', () => {
     console.log('add clicked');
     console.log(todoCreate.value);
 
     let input = document.createElement('p');
-    input.innerHTML = `<input value=${todoCreate.value} disabled> <i class="fa-solid fa-pen"></i> <i class="fa-sharp fa-solid fa-check"></i> <i class="fa-solid fa-trash"></i>`;
+    input.innerHTML = `<input value=${todoCreate.value} disabled> 
+            <button  onclick="editFunc()"> Edit </button> 
+            <button onclick="correctFunc()"> SAVE </button> 
+            <button onclick="deleteFunc(this)"> DEL </button>`;
 
     todoList.appendChild(input);
 
 });
+
+
