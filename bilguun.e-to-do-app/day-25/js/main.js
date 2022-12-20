524beace8854bc606e7b41a10a5a12e137d17866
Morning <morning@admins-iMac-3.local>
2022-12-20 12:26:23 +0800

    12/20/2022 main

--- bilguun.e-to-do-app/day-25/js/main.js
+++ bilguun.e-to-do-app/day-25/js/main.js
@@ -12,17 +12,75 @@ function printMeToDom(x, y) {
     y.innerHTML = x
 }
 
-setTimeout(() => {
-    printMeToDom("Зураг авах", p1);
-    setTimeout(() => {
-        printMeToDom("Амжилттай бол зургийг өөрчлөх", p2);
+// setTimeout(() => {
+//     printMeToDom("Зураг авах", p1);
+//     setTimeout(() => {
+//         printMeToDom("Амжилттай бол зургийг өөрчлөх", p2);
+//         setTimeout(() => {
+//             printMeToDom("Амжилттай бол амжилттай хадгалах", p3);
+//             setTimeout(() => {
+//                 printMeToDom("Амжилттай бол 'Амжилттай хадгалагдлаа' гэж хэвлэх", p4)
+//             }, timer4);
+//         }, timer3);
+//     }, timer2);
+// }, timer1);
+
+function callFirst(timer, element) {
+    const startPromise = new Promise((resolve, reject) => {
         setTimeout(() => {
-            printMeToDom("Амжилттай бол амжилттай хадгалах", p3);
-            setTimeout(() => {
-                printMeToDom("Амжилттай бол 'Амжилттай хадгалагдлаа' гэж хэвлэх", p4)
-            }, timer4);
-        }, timer3);
-    }, timer2);
-}, timer1);
+            if (element !== undefined) {
+                resolve(printMeToDom("Зураг авах", element))
+            } else {
+                reject("Something wrong has happened")
+            }
+        }, timer)
+    })
+
+    return startPromise
+}
+
+function callSecond(timer, element) {
+    const startPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (element !== undefined) {
+                resolve(printMeToDom("Амжилттай бол зургийг өөрчлөх", element))
+            } else {
+                reject("Something wrong has happened")
+            }
+        }, timer)
+    })
+
+    return startPromise
+}
+function callThird(timer, element) {
+    const startPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (element !== undefined) {
+                resolve(printMeToDom("Амжилттай бол амжилттай хадгалах", element))
+            } else {
+                reject("Something wrong has happened")
+            }
+        }, timer)
+    })
+
+    return startPromise
+}
+function callFourth(timer, element) {
+    const startPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (element !== undefined) {
+                resolve(printMeToDom("Амжилттай бол 'Амжилттай хадгалагдлаа' гэж хэвлэх", element))
+            } else {
+                reject("Something wrong has happened")
+            }
+        }, timer)
+    })
+
+    return startPromise
+}
 
 
+callFirst(timer1, p1).then(result => console.log(p1))
+callSecond(timer2, p2).then(result => console.log(p2))
+callThird(timer3, p3).then(result => console.log(p3))
+callFourth(timer4, p4).then(result => console.log(p4))
--- bilguun.e-to-do-app/day-25/js/promise.js
+++ bilguun.e-to-do-app/day-25/js/promise.js
@@ -18,8 +18,62 @@ const doPromise = new Promise((resolve, reject) => {
 
 console.log(doPromise)
 
-const res = doPromise.then((result) => {
-    console.log(result)
-})
+// doPromise
+//     .then(result => {
+//         console.log(result)
+//     })
+//     .catch(error => console.log(error))
 
-console.log(doPromise)
+function getImage(image) {
+    let imagePromise = new Promise((resolve, reject) => {
+        if (image.type != "png") {
+            reject("PNG файл биш байна");
+        } else {
+            resolve(image);
+        }
+    });
+    return imagePromise;
+}
+
+const img = {
+    imageName: "My image",
+    type: "png",
+    size: "1800x2090",
+}
+
+// const imageResult = getImage(img)
+// imageResult.then(result => {
+//     console.log(result)
+// })
+//     .catch(error => console.log(error))
+
+
+function reSizeImage(rightImage) {
+    let rightImagePromise = new Promise((resolve, reject) => {
+        if (rightImage.size != "1800x2090") {
+            reject("зөв хэмжээтэй файл биш байна.");
+        } else {
+            let resizedImage = rightImage;
+            resolve(resizedImage);
+        }
+    });
+    return rightImagePromise;
+}
+
+function saveImage(resizedImage) {
+    let saveImagePromise = new Promise((resolve, reject) => {
+        if (resizedImage == undefined) {
+            reject("Алдаа гарлаа.");
+        } else {
+            let saveImage = resizedImage;
+            resolve(saveImage);
+        }
+    });
+    return saveImagePromise;
+}
+
+getImage(img)
+    .then((image) => reSizeImage(image))
+    .then((reSizedImage) => saveImage(reSizedImage))
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
