21fa369161c62a7af264070e1c5aa20260d0f8d1
Morning <morning@admins-iMac-3.local>
2022-12-20 12:37:54 +0800

    class

--- sodoo-to-do-app/day25/js/promise.js
+++ sodoo-to-do-app/day25/js/promise.js
@@ -11,8 +11,8 @@ console.log(promise);
 // Promise
 const doPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
-        const skills = ["HTML", "CSS", "JS", "React"];
-        // const skills = [];
+        // const skills = ["HTML", "CSS", "JS", "React"];
+        const skills = [];
         if (skills.length > 0) {
             resolve(skills);
         } else {
@@ -21,11 +21,79 @@ const doPromise = new Promise((resolve, reject) => {
     }, 2000);
 });
 
-console.log(doPromise);
+doPromise
+    .then(result => {
+        console.log(result)
+    })
+    .catch(error => console.log(error))
 
 
-const res = doPromise.then((result) => {
-    return result;
-})
+// console.log(doPromise);
 
-console.log(res);
+
+// const res = doPromise.then((result) => {
+//     return result;
+// })
+
+// console.log(res);
+
+
+
+
+const img = {
+    imageName: 'My Image',
+    type: 'png',
+    size: '1800x2090',
+}
+
+const imageResult = getImage(img);
+imageResult
+    .then(result => {
+        console.log('png mun bn');
+    })
+    .catch(error => console.log('png bish bn'));
+
+
+
+
+function getImage(image) {
+    let imagePromise = new Promise((resolve, reject) => {
+        if (image.type != "png") {
+            reject("PNG файл биш байна");
+        } else {
+            resolve(image);
+        }
+    });
+    return imagePromise;
+}
+
+function reSizeImage(rightImage) {
+    let rightImagePromise = new Promise((resolve, reject) => {
+        if (rightImage.size != "1800x2090") {
+            reject("зөв хэмжээтэй файл биш байна.");
+        } else {
+            let resizedImage = rightImage;
+            resolve(resizedImage);
+        }
+    });
+    return rightImagePromise;
+}
+
+function saveImage(resizedImage) {
+    let saveImagePromise = new Promise((resolve, reject) => {
+        if (resizedImage == undefined) {
+            reject("Алдаа гарлаа.");
+        } else {
+            let saveImage = resizedImage;
+            resolve(saveImage);
+        }
+    });
+    return saveImagePromise;
+}
+
+
+getImage(img)
+    .then((image) => reSizeImage(image))
+    .then((reSizedImage) => saveImage(reSizedImage))
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
