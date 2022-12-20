5b08d28352df11e49236af54f9ff9dc13b83ecca
Morning <morning@Lab-02-01s-iMac.local>
2022-12-20 12:26:23 +0800

    day-25 done

---
+++ khuslenToDoApp/day-25/part1/js/main.js
@@ -0,0 +1,65 @@
+
+let p1 = document.getElementById("pa1");
+let p2 = document.getElementById("pa2");
+let p3 = document.getElementById("pa3");
+let p4 = document.getElementById("pa4");
+
+let Rdm1 = Math.floor(Math.random() * 1000);
+let Rdm2 = Math.floor(Math.random() * 1000);
+let Rdm3 = Math.floor(Math.random() * 1000);
+let Rdm4 = Math.floor(Math.random() * 1000);
+
+function printMeToDom(star, asd) {
+    asd.innerHTML = star;
+}
+
+
+function firstPromise() {
+    let FP = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            printMeToDom("Зураг авах", p1);
+        }, Rdm1);
+    });
+    return FP
+}
+
+function secondPromise() {
+    let SP = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            printMeToDom("Амжилттай бол зургийг өөрчлөх", p2);
+        }, Rdm2)
+    });
+    return SP
+}
+function thirdPromise() {
+    let TP = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            printMeToDom("Амжилттай бол амжилттай хадгалах", p3);
+        }, Rdm3)
+    });
+    return TP
+}
+
+function fourthPromise() {
+    let FoP = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            printMeToDom("Амжилттай хадгалагдлаа", p4)
+        }, Rdm4)
+    });
+    return FoP
+
+}
+
+
+firstPromise()
+    .then(FP => secondPromise())
+    .then(SP => thirdPromise())
+    .then(TP => fourthPromise())
+
+
+// const firstPromise = new Promise((resolve, reject))=> {
+//     setTimeout(() => {
+//         resolve()
+//     })
+// }
+// firstPromise.then
