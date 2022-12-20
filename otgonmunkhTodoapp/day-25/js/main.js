cd9fd498990c94d7b8c63b079d1f8c6ee14949c8
Morning <morning@mtars-lab2-17s-iMac.local>
2022-12-20 12:27:25 +0800

    day25

--- otgonmunkhTodoapp/day-25/js/main.js
+++ otgonmunkhTodoapp/day-25/js/main.js
@@ -14,24 +14,65 @@ let c = Math.floor(Math.random() * 1000);
 let d = Math.floor(Math.random() * 1000);
 
 
-function printMeToDom(name,  element ){
-    element.innerHTML = name;
-     
+function printMeToDom(value, element) {
+    element.innerHTML = value;
+
 
 }
-setTimeout(() => {
-    printMeToDom("take a picture", first);
-    setTimeout(() => {
-        printMeToDom("change a pic", second);
+
+// setTimeout(() => {
+//     printMeToDom("take a picture", first);
+
+// })
+
+// setTimeout(() => {
+//     printMeToDom("change a pic", second);
+
+// });
+
+
+// setTimeout(() => {
+//     printMeToDom("save pic", third);
+
+// })
+// setTimeout(() => {
+//     printMeToDom("successfull saved", fourth);
+// })
+
+function effect(first) {
+    let rightPromise = new Promise((resolve, reject) => {
         setTimeout(() => {
-            printMeToDom("save pic", third);
-            setTimeout(() => {
-                printMeToDom("successfull save", fourth);
-            }, d)
+            if (first == undefined) {
+                reject("Something wrong")
+            } else {
+                resolve(printMeToDom("Take a pic ", first))
+            }
+        }, 5000)
+    });
+    // return rightPromise;
+    rightPromise
+        .then(result => {
+            console.log(result);
+        }).catch(error => console.log(error))
+}
+effect(first);
+function change(second) {
+    let changePromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (first == undefined) {
+                reject("Something wrong")
+            } else {
+                resolve(printMeToDom("Change a pic ", second))
+            }
+        }, 5000)
+    });
+    changePromise
+        .then(result => {
+            console.log(result);
+        }).catch(error => console.log(error))
+}
+change(second);
 
-        }, c)
-    }, b);
 
-}, a)
 
 
--- otgonmunkhTodoapp/day-25/js/promis.js
+++ otgonmunkhTodoapp/day-25/js/promis.js
@@ -7,15 +7,77 @@ console.log(promise);
 // Promise
 const doPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
-    const skills = ["HTML", "CSS", "JS", "React"];
-    if (skills.length > 0) {
-        resolve(skills);
-    } else {
-        reject("Something wrong has happened");
-    }
+        const skills = [];
+        if (skills.length > 0) {
+            resolve(skills);
+        } else {
+            reject("Something wrong has happened");
+        }
     }, 2000);
 });
- 
-console.log(doPromise);
- const res = doPromise.then((result) => console.log(result));
-console.log(res);
+
+
+doPromise
+    .then(result => {
+        console.log(result)
+    })
+    .catch(error => console.log(error))
+
+// console.log(doPromise);
+//  const res = doPromise.then((result) => console.log(result));
+// console.log(res);
+// GET Image
+function getImage(image) {
+    let imagePromise = new Promise((resolve, reject) => {
+        if (image.type != "png") {
+            reject("PNG файл биш байна");
+        } else {
+            resolve(image);
+        }
+    });
+    return imagePromise;
+}
+
+const img = {
+    imageName: "My image",
+    type: "png",
+    size: "800x2090"
+}
+const imageResult = getImage(img);
+console.log(imageResult);
+
+// imageResult
+//      .then(result => {
+//         console.log(result);
+//      }).catch(error => console.log(error))
+
+
+
+function reSizeImage(rightImage) {
+    let rightImagePromise = new Promise((resolve, reject) => {
+        if (rightImage.size != "1800x2090") {
+            reject("зөв хэмжээтэй файл биш байна.");
+        } else {
+            let resizedImage = rightImage;
+            resolve(resizedImage);
+        }
+    });
+    return rightImagePromise;
+}
+function saveImage(resizedImage) {
+    let saveImagePromise = new Promise((resolve, reject) => {
+        if (resizedImage == undefined) {
+            reject("Алдаа гарлаа.");
+        } else {
+            let saveImage = resizedImage;
+            resolve(saveImage);
+        }
+    });
+    return saveImagePromise;
+}
+
+getImage(img)
+    .then((image) => reSizeImage(image))
+    .then((reSizedImage) => saveImage(reSizedImage))
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
