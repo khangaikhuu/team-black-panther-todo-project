f15462064598c8411a600a321e0952019f15c479
Morning <morning@Lab-02-01s-iMac.local>
2022-12-21 12:30:35 +0800

    day-26 undone

---
+++ khuslenToDoApp/day-26/js/main.js
@@ -0,0 +1,46 @@
+
+// let promiseFullfilled = false;
+// let changingVariable;
+// console.log("first")
+// console.log(changingVariable)
+// console.log("first")
+
+// const myPromise = new Promise((resolved, rejected) => {
+
+//     if (promiseFullfilled) {
+//         console.log("second");
+
+//         changingVariable = "Promise is fullfilled";
+//         console.log(changingVariable)
+
+//         console.log("second");
+
+//         resolved(changingVariable);
+
+//     } else {
+//         rejected("Promise is not fullfilled")
+//     }
+// })
+
+// myPromise
+//     .then(result => {
+//         console.log("third")
+//         console.log(result)
+//         console.log("third")
+//     }).catch(e => {
+//         console.log(e)
+//     })
+
+//     console.log("fourth")
+//     console.log(changingVariable);
+//     console.log("fourth")
+
+// console.log("a");
+// console.log("b");
+// console.log("c");
+
+// for (let i = 0; i < 1_000_000; i++){
+//     for (let j = 0; j <1_000_000; j++){
+
+//     }
+// }, 5000; 
