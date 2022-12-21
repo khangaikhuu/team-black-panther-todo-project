d59dfb4072a86c224cdd724fcd5bb8d821cf1654
Morning <morning@mstars-lab2-08s-iMac.local>
2022-12-21 09:58:58 +0800

    Day 26 add

--- Dulguun-todo/Day-25/js/promise.js
+++ Dulguun-todo/Day-25/js/promise.js
@@ -8,22 +8,22 @@
 // console.log(promise);
 
 // Promise
-const doPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        const skills = ['array','two','three'];
-        if (skills.length > 0) {
-            resolve(skills);
-        } else {
-            reject("Something wrong has happened");
-        }
-    }, 1000);
-});
-console.log(doPromise);
+// const doPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         const skills = ['array', 'two', 'three'];
+//         if (skills.length > 0) {
+//             resolve(skills);
+//         } else {
+//             reject("Something wrong has happened");
+//         }
+//     }, 1000);
+// });
+// console.log(doPromise);
 
 
-doPromise.then(result => {
-    console.log(result)
-}).catch(error => console.log(error));
+// doPromise.then(result => {
+//     console.log(result)
+// }).catch(error => console.log(error));
 
 // //    GET Image
 // function getImage(image) {
@@ -48,38 +48,38 @@ doPromise.then(result => {
 // }).catch(error => console.log(error));
 
 // GET Image
-	function getImage(image) {
+async function getImage(image) {
     let imagePromise = new Promise((resolve, reject) => {
         if (image.type != "png") {
-        reject("PNG файл биш байна");
+            reject("PNG файл биш байна");
         } else {
-        resolve(image);
+            resolve(image);
         }
-    }); 
+    });
     return imagePromise;
 }
 
-function reSizeImage(rightImage) {
-   let rightImagePromise = new Promise((resolve, reject) => {
-       if (rightImage.size != "1800x2090") {
-       reject("зөв хэмжээтэй файл биш байна.");
-       } else {
-       let resizedImage = rightImage;
-       resolve(resizedImage);
-       }
-   });
-   return rightImagePromise;
+async function reSizeImage(rightImage) {
+    let rightImagePromise = new Promise((resolve, reject) => {
+        if (rightImage.size != "1800x2090") {
+            reject("зөв хэмжээтэй файл биш байна.");
+        } else {
+            let resizedImage = rightImage;
+            resolve(resizedImage);
+        }
+    });
+    return rightImagePromise;
 }
-function saveImage(resizedImage) {
-   let saveImagePromise = new Promise((resolve, reject) => {
-       if (resizedImage == undefined) {
-       reject("Алдаа гарлаа.");
-       } else {
-       let saveImage = resizedImage;
-       resolve(saveImage);
-       }
-   });
-   return saveImagePromise;
+async function saveImage(resizedImage) {
+    let saveImagePromise = new Promise((resolve, reject) => {
+        if (resizedImage == undefined) {
+            reject("Алдаа гарлаа.");
+        } else {
+            let saveImage = resizedImage;
+            resolve(saveImage);
+        }
+    });
+    return saveImagePromise;
 }
 
 const img = {
@@ -88,9 +88,8 @@ const img = {
     size: '1800x2090'
 }
 
-
 getImage(img)
-   .then((image) => reSizeImage(image))
-   .then((reSizedImage) => saveImage(reSizedImage))
-   .then((res) => console.log(res))
-   .catch((err) => console.log(err));
+    .then((image) => reSizeImage(image))
+    .then((reSizedImage) => saveImage(reSizedImage))
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
