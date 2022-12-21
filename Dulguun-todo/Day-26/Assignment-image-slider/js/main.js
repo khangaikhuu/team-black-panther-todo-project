a00cfc38d7fed181eb15da97efe1283bb292f4f4
Morning <morning@mstars-lab2-08s-iMac.local>
2022-12-21 12:49:42 +0800

    added new img slide version

--- Dulguun-todo/Day-26/Assignment-image-slider/js/main.js
+++ Dulguun-todo/Day-26/Assignment-image-slider/js/main.js
@@ -1,23 +1,35 @@
 const imgContainer = document.getElementById('container');
 
+//B.Bilguun version
+// function setWidth300px(element) {
+//     for (let i = 0; i < element.children.length; i++) {
+//         element.children[i].style = 'width: 300px';
+//     }
+// }
 
-function setWidth300px(element) {
-    for (let i = 0; i < element.children.length; i++) {
-        element.children[i].style = 'width: 300px';
-    }
+// function slide (element) {
+//     for (let n = 0; n <element.children.length; n++) {
+//         element.children[n].addEventListener('click', (self) => {
+//             setWidth300px(imgContainer)
+//             self.target.style = 'width: 800px'
+//         })
+//     }
+// }
+// imgContainer.addEventListener('click', slide(imgContainer));
+
+//Khongorzul version
+let imgContainerChildren = imgContainer.children;
+
+for(let i = 0; i < imgContainerChildren.length; i++){
+    imgContainerChildren[i].addEventListener("click", (self) => {
+        for(let j = 0; j < imgContainerChildren.length; j++){
+            if(j !== i){
+                imgContainerChildren[j].style = "width: 300px";
+            }
+        }
+        self.target.style = "width: 800px";
+    })               
 }
-
-function slide (element) {
-    for (let n = 0; n <element.children.length; n++) {
-        element.children[n].addEventListener('click', (self) => {
-            setWidth300px(imgContainer)
-            self.target.style = 'width: 800px'
-        })
-    }
-}
-imgContainer.addEventListener('click', slide(imgContainer));
-
-
 
 
 
