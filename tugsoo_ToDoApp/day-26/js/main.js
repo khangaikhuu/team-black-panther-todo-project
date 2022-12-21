7101a0485fe7d770a0adb562c57293a2fcbfca7a
Morning <morning@Mstarss-iMac-2.local>
2022-12-21 12:45:22 +0800

    day-26

---
+++ tugsoo_ToDoApp/day-26/js/main.js
@@ -0,0 +1,18 @@
+// dfgdfgdfg
+
+
+console.log("a");
+console.log("b");
+console.log("c");
+
+setTimeout(()=>{
+    for (let i = 0; i < 1_000_000; i++){
+        for (let j = 0; j < 1_000_000; j++){
+    
+        }
+    }
+},5000)
+
+console.log("final");
+
+
