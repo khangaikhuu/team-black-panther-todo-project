3a6407b05a5f52a94bc88a4f73215d374c39393d
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 <sodoo75@gmail.com>
2022-12-22 16:52:17 +0800

    update

--- sodoo-to-do-app/anime-platform/js/main.js
+++ sodoo-to-do-app/anime-platform/js/main.js
@@ -15,6 +15,8 @@ fetch(fullMetalAlchemistURL)
         document.getElementById('manga-container').getElementsByTagName('h6')[0].textContent = data.data.titles[0].title
         const tv = document.getElementById('manga-container').getElementsByTagName('p')[0].textContent = data.data.type + ', ' + data.data.year + ' | ' + data.data.status.substring(0, 8) + ' | ' + data.data.episodes + ' eps, ' + data.data.duration.substring(0, 6);
 
+        const synopsis = document.getElementById('synopsis').textContent = data.data.synopsis
+
 
         const type = document.querySelector('#manga-container p')
         console.log(type);
