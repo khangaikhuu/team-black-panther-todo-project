1561f5216823f81c0a74fcc61ce9ae4d5a205d54
Morning <morning@Lab-02-02s-iMac.local>
2022-12-23 13:09:18 +0800

    12.13 at class

--- Jamiyan/day-27/anime-platform/js/main.js
+++ Jamiyan/day-27/anime-platform/js/main.js
@@ -7,7 +7,7 @@ fetch(fullMetalAlchemistURL)
         .then(data => {
             console.log(data);
             const mangaDomImage = document.getElementById("manga-image");
-            console.log(mangaDomImage)
+            
             mangaDomImage.src = data.data.images.jpg.image_url;
 
             document.getElementById("manga-container").getElementsByTagName("h6")[0].textContent = data.data.titles[0].title
@@ -19,5 +19,45 @@ fetch(fullMetalAlchemistURL)
             document.getElementById("drama").textContent = data.data.genres[2].name
             document.getElementById("fantasy").textContent = data.data.genres[3].name
 
-            document.getElementById("img-text").textContent = data.data.synopsis
+            
+            document.getElementById("img-text").textContent = data.data.synopsis.slice(0, 200)
+            let color = 1;
+            document.getElementById("down").addEventListener("click", () => {
+                if(color = 1){
+                document.getElementById("img-text").textContent = data.data.synopsis;
+                color = 2;
+                } else if(color = 2){
+                    document.getElementById("img-text").textContent = data.data.synopsis.slice(0, 200);
+                    color = 1;
+                }
+            })
+
+
+            document.getElementById("img-type1").textContent = `Studio: `
+            document.getElementById("img-type2").textContent = `Source: ${data.data.source}`
+            document.getElementById("img-type3").textContent = `Theme: `
+            document.getElementById("img-type4").textContent = `Demographic: `
+
+            const a1 = document.createElement("a");
+            a1.innerHTML = data.data.studios[0].name
+            a1.href = data.data.studios[0].url
+            document.getElementById("img-type1").appendChild(a1);
+
+            const a2 = document.createElement("a");
+            a2.innerHTML = data.data.themes[0].name
+            a2.href = data.data.themes[0].url
+            document.getElementById("img-type3").appendChild(a2);
+
+            const a3 = document.createElement("a");
+            a3.innerHTML = data.data.demographics[0].name
+            a3.href = data.data.demographics[0].url
+            document.getElementById("img-type4").appendChild(a3);
+
+            document.getElementById("score-number").textContent = data.data.score
+            document.getElementById("popularity").textContent = `${data.data.popularity}.0M `;
+            document.getElementById("add").textContent = `Add to List`
+
+            const iconStar = document.createElement("p1");
+            iconStar.innerHTML = ' <i class="fa-regular fa-star" style= "font-size: 16px"></i> '
+            document.getElementById("score").textContent.appendChild(iconStar)
         })
