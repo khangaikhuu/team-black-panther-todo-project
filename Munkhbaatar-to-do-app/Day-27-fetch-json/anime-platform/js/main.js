ee7aee2c0297bef86bad61fac53c9e432a696427
Morning <morning@MStarss-iMac.local>
2022-12-23 13:28:01 +0800

    anime exercise

--- Munkhbaatar-to-do-app/Day-27-fetch-json/anime-platform/js/main.js
+++ Munkhbaatar-to-do-app/Day-27-fetch-json/anime-platform/js/main.js
@@ -22,17 +22,57 @@ fetch(fullMetalAlchemistUrl)
         let episodes = res.data.episodes;
         let duration = res.data.duration.substring(0, 6);
         let genresTemp = res.data.genres;
-        let genres = "";
+       
+
+
+        document.querySelector('#manga-container #type').innerHTML = type + ',  ' + year +  "  |  " + status + 
+        "  |  " + episodes + 'eps, ' + duration;
         
-        document.querySelector('#manga-container #type').innerHTML = type + ', ' + year +  " | " + status + 
-        " | " + episodes + 'eps, ' + duration;
 
         genresTemp.map((element) => {
-            genres += element.name + " ";
-            
+
+            let content = document.querySelector('#genres');
+            let genre = document.createElement('a');
+            genre.href = '#'
+            genre.innerHTML = element.name;
+            content.appendChild(genre);
+
         })
-        document.querySelector('#genres').innerText = genres
-        console.log(genres);
+
+        let extendBtn = document.querySelector('.extend');
+        let fullTxt = document.querySelector('#synopsisFull');
+        let halfTxt = document.querySelector('#synopsisHalf');
+
+        fullTxt.style.display = 'none';
+        halfTxt.style.display = 'block'
+
+        extendBtn.addEventListener('click', () => {
+            if(fullTxt.style.display == 'none' ){
+                console.log('test1')
+                fullTxt.style.display = 'block'
+                halfTxt.style.display = 'none';
+                extendBtn.innerHTML = `<i class="bi bi-caret-up-fill"></i>`
+
+            } else {
+                fullTxt.style.display = 'none';
+                halfTxt.style.display = 'block';
+                console.log('test2')
+                extendBtn.innerHTML = `<i class="bi bi-caret-down-fill"></i>`
+            }
+        })
+
+        document.querySelector('#synopsisHalf').innerHTML = (res.data.synopsis).substring(0, 398);
+        document.querySelector('#synopsisFull').innerHTML = res.data.synopsis;
+
+        document.querySelector('#studio').innerHTML = '<b>Studio: </b>' + `<a href='${res.data.studios[0].url}' > ${res.data.studios[0].name}</a>`;
+        document.querySelector('#source').innerHTML = '<b>Source: </b>' + res.data.source;
+        document.querySelector('#theme').innerHTML = '<b>Theme</b>: ' + `<a href='${res.data.themes[0].url}'> ${res.data.themes[0].name}</a>`;
+        document.querySelector('#demographic').innerHTML = '<b>Demographic: </b>' + res.data.demographics[0].name;
+
+        
+        document.querySelector('#score').innerHTML = '<i class="bi bi-star"></i> ' + res.data.score;
+        document.querySelector('#member').innerHTML = '<i class="bi bi-person-fill"></i>' + (res.data.members/1.0e+6).toFixed(1) + " M";
+        
 
     })
 
