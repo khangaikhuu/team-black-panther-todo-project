153cb49f3af37f10cea16b8b37e09ce08857d24a
OtgonmunkhG <g.otgonmunkh@gmail.com>
2022-12-27 18:08:45 +0800

    added

--- otgonmunkhTodoapp/anime-platform/js/animes.js
+++ otgonmunkhTodoapp/anime-platform/js/animes.js
@@ -1,63 +1,15 @@
 console.log("hello world");
 
-// const numbers = [1, 2, 3, 4, 5];
-// numbers.filter(number => number < 3)
 const card = document.querySelector(".card");
-async function search(event) {
-    const searchField = document.getElementById("search-field");
-    const searchWord = searchField.value;
-    const animes = await fetch('https://api.jikan.moe/v4/top/anime')
-    const animesJSON = await animes.json();
-    const animesData = animesJSON.data;
 
-    const searchResult = animesData.filter(anime =>
-        anime.title.includes(searchWord)
-
-    )
-    console.log(searchResult);
-    const container = document.querySelector("#manga-container");
-    container.innerHTML = "" ;
-    searchResult.map((element, index) => {
-        console.log(element);
-        container.innerHTML += getAnimes(element, index)
-    })
-    console.log(searchResult);
-
-}
-async function showMore(event) {
-    const synop = document.getElementById(`synopsis${event.id}`);
-
-    const resultJSON = await fetch('https://api.jikan.moe/v4/top/anime');
-    const result = await resultJSON.json();
-    const animeData = result.data;
-
-    const filteredData = animeData.filter((el, index) => {
-        if (index == event.id) {
-            return el;
-        }
-
-    })
-
-    console.log(filteredData);
+function getAnimes(animes) {
 
 
-
-
-
-}
-
-function getAnimes(animes, index) {
-
-
-    const genres = animes.genres.map(genre => {
-        console.log(genre);
-        const result = `<p>${genre.name}</p>`;
-        return result;
-    })
-    const score = animes.score;
-    const members = animes.members;
-
-
+        const genres = animes.genres.map(genre => {
+            console.log(genre);
+            const result = `<p>${genre.name}</p>`;
+            return result;
+        })
 
 
     return `
@@ -65,48 +17,19 @@ function getAnimes(animes, index) {
          <a href="#" id="title" style="display:block">${animes.title}</a>
          <div class="intro">
              <i class="fa-sharp fa-solid fa-circle-play"></i>
-             <span id="type">${animes.type}, ${animes.year} | ${animes.status.substring(0, 8)} | ${animes.episodes} eps, ${animes.duration.substring(0, 6)}</span>
+             <span id="type">${animes.type}, ${animes.year} | ${animes.status.substring(0, 8)} | ${animes.episodes} eps, ${animes.duration.substring(0,6)}</span>
              <i class="fa-solid fa-tower-broadcast"></i>
              
 
          </div>
-         <div class="genres" >
+         <div class="genres" id="genres">
            ${genres.join('')}
          </div>
-
-         <div class="img-container">
-            <img src=${animes.images.jpg.image_url}>
-            <div class="p-con">
-                 <div class="first-p" id="synopsis_${index}">${animes.synopsis.slice(0, 369)}</div>
-                 <div class="second" ></div>
-                 <button id="${index}" onclick="showMore(this);">
-                    <i class="fa-sharp fa-solid fa-chevron-up"></i>
-                 </button>
-                 <button id="${index}" onclick="showMore(this);">
-                    <i class="fa-solid fa-chevron-down"></i>
-                 </button>
-                 <div class="source" >
-                   
-                 </div>
-                 
-
-            </div>
+         <div id="detail">
+             <p><p/>
+             <p class="second"></p>
          </div>
-        <div class="card-footer" >
-             <div class="score">
-             
-             <i class="fa-regular fa-star"></i>
-                 ${score}
-            </div>
-            <div class="members">
-            <i class="fa-solid fa-person"></i>
-                ${members}        
-            </div>
-            <div class="button">
-                <button>Add to List</button>       
-            </div>
-            
-        </div>
+         <img src=${animes.images.jpg.image_url}>
          
     </div>
     `
@@ -117,19 +40,9 @@ fetch(animesURL)
     .then(animes => {
         console.log(animes.data);
         const container = document.querySelector("#manga-container");
-
-        animes.data.map((element, index) => {
+        console.log(container);
+        animes.data.map(element => {
             console.log(element);
-            container.innerHTML += getAnimes(element, index)
+            container.innerHTML += getAnimes(element)
         })
-
-
-
-
-
-
-
-
-
-
     })
