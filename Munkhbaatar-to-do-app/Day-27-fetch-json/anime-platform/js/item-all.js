d9c5d1e5a5723dc7cd3381d308e3d208d4bdbc4a
Morning <morning@MStarss-iMac.local>
2022-12-29 10:16:38 +0800

    test

--- Munkhbaatar-to-do-app/Day-27-fetch-json/anime-platform/js/item-all.js
+++ Munkhbaatar-to-do-app/Day-27-fetch-json/anime-platform/js/item-all.js
@@ -75,7 +75,7 @@ const select = document.getElementById('genre')
 select.addEventListener('change', function handleChange(event) {
     console.log(event.target.value); // 👉️ get selected VALUE
 
-    // // 👇️ get selected VALUE even outside event handler
+    // 👇️ get selected VALUE even outside event handler
     // console.log(select.options[select.selectedIndex].value);
 
     // // 👇️ get selected TEXT in or outside event handler
