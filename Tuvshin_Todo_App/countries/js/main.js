1de58d0130a1a0d8a819eb8bb3378fbdf6a2c9c0
Morning <morning@MStarss-iMac.local>
2023-01-04 12:24:23 +0800

    countries

--- Tuvshin_Todo_App/countries/js/main.js
+++ Tuvshin_Todo_App/countries/js/main.js
@@ -1,8 +1,24 @@
 fetch('https://restcountries.com/v3.1/all')
     .then(res => res.json())
     .then((data) => {
-        console.log(data.name);
-        const allCounties = data.allCounties;
+        // console.log(data);
+        const allCountries = data;
+        console.log(allCountries);
+
+        const container = document.querySelector("#main-country");
+        container.innerHTML = " ";
+        container.className = "mainCountry";
         
+        allCountries.map(element) => {
+            const countryName = document.createElement("h3");
+            countryName.innerHTML = element.name;
+
+            container.appendChild(countryName);
+
+
+        }
+
+        
+        // eachProduct.appendChild(phoneTitle);
 
     });
